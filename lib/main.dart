ca3ded2798f9c3222594ca1b008ab4f98529637a
corgiz4lif <73208066+corgiz4lif@users.noreply.github.com>
2023-09-18 12:58:38 -0400

    super basic calendar impemented from https://pub.dev/packages/table_calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(MyApp());
@@ -70,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
       case 3:
         page = SavedNotesPage();
       case 4:
-        page = Placeholder();
+        page = CalendarPage();
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
@@ -273,7 +274,7 @@ class _NewNotePageState extends State<NewNotePage> {
                       ],
                     );
                   },
-                                  );
+                );
               },
             )
           ],
@@ -291,6 +292,63 @@ class SavedNotesPage extends StatelessWidget {
         children: [
           Text('Saved Notes'),
         ],
+      ),
+    );
+  }
+}
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({super.key});
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  final kFirstDay = DateTime.utc(2021, 1, 1);
+  final kLastDay = DateTime.utc(2030, 12, 31);
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TableCalendar - Basics'),
+      ),
+      body: TableCalendar(
+        firstDay: kFirstDay,
+        lastDay: kLastDay,
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          // Use `selectedDayPredicate` to determine which day is currently selected.
+          // If this returns true, then `day` will be marked as selected.
+
+          // Using `isSameDay` is recommended to disregard
+          // the time-part of compared DateTime objects.
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            // Call `setState()` when updating the selected day
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          }
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
       ),
     );
   }
