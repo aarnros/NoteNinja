2387af70be176b097994694f5c86e1c78989a5bd
Dan Johnson <jdjohnson8883@gmail.com>
2023-11-02 13:46:16 -0400

    Rebased and set up firebase with config files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart';
 import 'pages/sign_up_page.dart';
 import 'pages/home_page.dart';
 
-void main() => runApp(const SignUpApp());
+Future<void> main() async{
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const SignUpApp());
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
